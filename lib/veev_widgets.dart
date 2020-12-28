2ac044079ecbea35e6a42c34df9225a7d69061f2
Isac Martins <50426537+isaukywhite@users.noreply.github.com>
2020-12-28 15:22:19 -0300

    alt f4

--- lib/veev_widgets.dart
+++ lib/veev_widgets.dart
@@ -5,3 +5,5 @@ export 'src/custom_veev_buttom/custom_veev_button_controller.dart';
 
 export 'src/custom_veev_textinput/custom_veev_textinput.dart';
 export 'src/custom_veev_textinput/custom_veev_textinput_controller.dart';
+
+export 'src/custom_scaffold/custom_scaffold.dart';
