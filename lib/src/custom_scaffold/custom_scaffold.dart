47cc6a387fbdfe053c99044e37b6030f306351c3
Isac Martins <50426537+isaukywhite@users.noreply.github.com>
2020-12-28 15:25:40 -0300

    comentarios

--- lib/src/custom_scaffold/custom_scaffold.dart
+++ lib/src/custom_scaffold/custom_scaffold.dart
@@ -3,9 +3,26 @@ import 'package:bitsdojo_window/bitsdojo_window.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-///antes de usar esse widget lembrar de colocar em windows/runner/win32_windows.cpp
+///antes de usar esse widget lembrar de modificar:
+///
+///..windows/runner/win32_windows.cpp
 ///case WM_CLOSE:
 ///  return 0;
+///
+///..windows/runner/main.cpp
+/// #include <bitsdojo_window/bitsdojo_window_plugin.h>
+/// auto bdw = bitsdojo_window_configure(BDW_CUSTOM_FRAME|BDW_HIDE_ON_STARTUP);
+///
+/// main.dart
+/// doWhenWindowReady(() {
+///    var initialSize = Size(600, 450);
+///    appWindow.minSize = initialSize;
+///    appWindow.size = initialSize;
+///    appWindow.alignment = Alignment.center;
+///    appWindow.maximizeOrRestore();
+///    appWindow.show();
+///  });
+///
 
 class CustomScaffold extends StatelessWidget {
   final Widget widget;
